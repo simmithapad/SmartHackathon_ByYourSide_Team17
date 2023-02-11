48b41ab986b82c6c60a010f8be19c4f0034f5287
simmithapad <simmithapad23@gmail.com>
2023-02-11 22:26:33 +0530

    initial commit

---
+++ login.js
@@ -0,0 +1,11 @@
+const signUpButton = document.getElementById('signUp2');
+const signInButton = document.getElementById('signIn2');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () =>
+container.classList.add('right-panel-active'));
+
+signInButton.addEventListener('click', () =>
+container.classList.remove('right-panel-active'));
+
+
